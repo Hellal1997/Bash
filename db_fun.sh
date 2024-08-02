4fc185273f82288552044a34d85bbf715f9e6ab3
mohamed <mohamedsaad01095@gmail.com>
2024-08-02 09:21:24 +0300

    add my function db

---
+++ db_fun.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+list_tables(){
+	"welcome to my list"
+}
+create_table(){
+	"welcome to create table"
+}
+select_from_table(){
+	"welcome to select table"
+}
+insert_into_table(){
+	"welcome to insert table"
+}
+delete_from_table(){
+	"welcome to delete table"
+}
+upate_table(){
+	"you are welcome to update table"
+}
+drop_table(){
+	"welcome to drop table"
+}
